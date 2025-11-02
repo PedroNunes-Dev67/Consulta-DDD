f7f4c265f60607756ee52ca43c83712ab2b54984
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2025-11-02 11:26:19 -0300

    Conectando o frontend ao backend

--- backend/ConsultaDDD/src/main/java/PedroNunesDev/ConsultaDDD/controller/DDDController.java
+++ backend/ConsultaDDD/src/main/java/PedroNunesDev/ConsultaDDD/controller/DDDController.java
@@ -1,14 +1,17 @@
 package PedroNunesDev.ConsultaDDD.controller;
 
-import PedroNunesDev.ConsultaDDD.model.DDD;
-import PedroNunesDev.ConsultaDDD.service.DDDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import PedroNunesDev.ConsultaDDD.model.DDD;
+import PedroNunesDev.ConsultaDDD.service.DDDService;
+
+@CrossOrigin(origins= "http://localhost:5500")
 @RestController
 @RequestMapping("/ddd")
 public class DDDController {
@@ -19,6 +22,8 @@ public class DDDController {
     @GetMapping("/{ddd}")
     public ResponseEntity<DDD> buscarDDD(@PathVariable("ddd") String dddRequest){
 
-        return dddService.buscarDDD(dddRequest);
+        ResponseEntity<DDD> response = dddService.buscarDDD(dddRequest);
+
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
     }
 }
---
+++ frontend/js/script.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+const stateResp = document.getElementById('stateResp');
+const citiesResp = document.getElementById('citiesResp');
+
+form.addEventListener('submit', async (e) => {
+    
+    e.preventDefault();
+
+    const ddd = document.getElementById('inDDD').value;
+
+    
+    let response = await axios.get(`http://localhost:8080/ddd/${ddd}`);
+
+    let {cities, state} = response.data;
+    stateResp.innerText="Estado: "+state;
+    let text = "";
+
+    cities.forEach(element => {
+       
+        text+=element+'\n';
+    });
+
+    citiesResp.innerText=text;
+})
