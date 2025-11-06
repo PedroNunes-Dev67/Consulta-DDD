63e1a904d5b4e63d60ef6c7bd61c286d6d36c23e
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2025-11-05 21:08:23 -0300

    Criando o index.html do projeto

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -9,16 +9,24 @@ form.addEventListener('submit', async (e) => {
     const ddd = document.getElementById('inDDD').value;
 
     
-    let response = await axios.get(`http://localhost:8080/ddd/${ddd}`);
+    try{
+        let response = await axios.get(`http://localhost:8080/ddd/${ddd}`);
+     
+        let {cities, state} = response.data;
+        stateResp.innerText="Estado: "+state;
+        let text = "";
 
-    let {cities, state} = response.data;
-    stateResp.innerText="Estado: "+state;
-    let text = "";
-
-    cities.forEach(element => {
+        cities.forEach(element => {
        
-        text+=element+'\n';
-    });
+            text+=element+'\n';
+        });
 
     citiesResp.innerText=text;
+    }
+    catch(error){
+        alert('DDD não encontrado');
+        form.inDDD.value = "";
+        stateResp.innerText= ""
+        citiesResp.innerText=""
+    }
 })
